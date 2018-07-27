f0eb510360d9d118ce874e555520c5383b850f9c
mcverter <mitchell.verter@gmail.com>
2018-07-26 23:17:24 -0400

    react native file

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import React, { Component } from 'react';
+//import {Provider} from 'react-redux';
+
+import Router from './src/routes';
+//import configureStore from './redux/store/configureStore'
+//const initialState = require('./redux/store/initialState');
+//const store = configureStore(initialState);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+
+  }
+  render() {
+    return (
+  //    <Provider store={store}>
+        <Router/>
+  //    </Provider>
+    );
+  }
+}
+
+export default App;
